fdfb2c1cf404f88eb1b74d091dfcb1f6d6e3e452
André Restivo <andre.restivo@gmail.com>
2014-01-27 07:35:40 +0900

    Fine test

--- GitTest/src/test.java
+++ GitTest/src/test.java
@@ -3,7 +3,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I hope this works1111!!");
+		System.out.println("edited from mah androif");
 	}
 
 }
