0a98ba3cc092bbaf22a63671790ab58838f208fb
cameronfr <catechboy@hotmail.com>
2014-01-27 07:54:20 +0900

    Fine test two

--- GitTest/src/test.java
+++ GitTest/src/test.java
@@ -3,7 +3,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("edited from mah android");
+		System.out.println("edited from mah android2");
 	}
 
 }
