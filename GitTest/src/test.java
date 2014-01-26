824eb6772545e051761c5633c01963593981b1bf
André Restivo <andre.restivo@gmail.com>
2014-01-27 07:45:01 +0900

    Dafuk

--- GitTest/src/test.java
+++ GitTest/src/test.java
@@ -3,7 +3,8 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("edited from mah androif");
+		System.out.println("edited from mah android");
 	}
 
 }
+ 
