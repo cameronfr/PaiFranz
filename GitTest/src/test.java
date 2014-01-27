059d3512e1efb4a52c255f780de37e3930362830
cameronfr <catechboy@hotmail.com>
2014-01-27 09:06:11 +0900

    Naveen come back

--- GitTest/src/test.java
+++ GitTest/src/test.java
@@ -3,7 +3,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("edited from mah android2");
+		System.out.println("Sent from my Android");
 	}
 
 }
