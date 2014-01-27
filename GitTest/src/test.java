f0e9448d5e3fb8a9f83c34f8149e03632c740ad6
cameronfr <catechboy@hotmail.com>
2014-01-27 10:46:01 +0900

    another testy

--- GitTest/src/test.java
+++ GitTest/src/test.java
@@ -1,9 +1,8 @@
-
 public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Sent from my Android");
+		System.out.println("Hello Githubz");
 	}
 
 }
