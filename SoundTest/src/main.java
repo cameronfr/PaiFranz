cb6fb542b40fe76747762d8ef357d9f6af8d6527
Naveen <pai.naveen357@gmail.com>
2014-01-29 22:16:58 -0800

    Create stupid program that uses Buffer Reader

--- SoundTest/src/main.java
+++ SoundTest/src/main.java
@@ -1,12 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
  
 public class main {
 
-	public static void main(String[] args) {
-		System.out.println("Sound control panel: ");
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Sound Panel: ");
+        String s = br.readLine();
+        
+        if (s.equals("a")) invokeSound();
 		
-		if (System.in.equals('a')) {
-			invokeSound()
-		}
 	}
 	
 	public static void invokeSound() {
