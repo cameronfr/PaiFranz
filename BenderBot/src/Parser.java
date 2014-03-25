553d380c3b935cb691ae8cb551a2c2aec5f9e582
cameronfr <catechboy@hotmail.com>
2014-03-25 19:54:24 +0900

    make the parser dyanmic in terms of key length/markov order

--- BenderBot/src/Parser.java
+++ BenderBot/src/Parser.java
@@ -7,39 +7,61 @@ import java.util.Scanner;
 
 public class Parser {
 
-	HashMap<String,ArrayList<String>> table;
-	int keyLength;
-	
-	public Parser(int keyLengthIn) {
-		keyLength = keyLengthIn;
-		table = new HashMap<String,ArrayList<String>>();
+	HashMap<String, ArrayList<String>> table;
+	int markovOrder;
+
+	public Parser(int markovOrderIn) {
+		markovOrder = markovOrderIn;
+		table = new HashMap<String, ArrayList<String>>();
 	}
 
 	public void parseFile(String path) throws FileNotFoundException {
 		String currentDirectory = new File("").getAbsolutePath();
-		Scanner text = new Scanner(new File(currentDirectory+ path));
+		Scanner text = new Scanner(new File(currentDirectory + path));
 		simpleReadText(text);
 	}
-	
-	//reads from a file with formatted sentences, 2 values 
+
+	// reads from a file with formatted sentences, 2 values
 	private void simpleReadText(Scanner text) {
-			String word1,word2,word3;
-			word1 = text.next();
-			word2 = text.next();
-			while(text.hasNext()) {
-				word3 = text.next();
-				String key = word1.concat(" ").concat(word2);
-				String value = word3;
-				addEntry(key.trim(), value.trim());
-				word1 = word2;
-				word2 = word3;
+		int wordBufferLength = markovOrder + 1;
+		String[] wordBuffer = new String[markovOrder + 1];
+		for (int i = 0; i < wordBufferLength; i++) {
+			if (text.hasNext())
+				wordBuffer[i] = text.next();
+		}
+		while (text.hasNext()) {
+			String key = "";
+			String value = wordBuffer[wordBufferLength-1];
+			//last word in buffer is value, not part of key
+			for (int i = 0; i<wordBufferLength-1; i++) {
+				key = key.concat(" ").concat(wordBuffer[i]);
 			}
-			
-		
+			addEntry(key.trim(), value.trim());
+			for (int i = 0; i < wordBufferLength-1; i++) {
+				wordBuffer[i] = wordBuffer[i+1];
+			}
+			wordBuffer[markovOrder] = text.next();
+		}
+
+		/*
+		 * String word1,word2,word3;
+		 *  word1 = text.next(); 
+		 *  word2 = text.next();
+		 * while(text.hasNext()) { 
+		 * word3 = text.next(); 
+		 * String key = word1.concat(" ").concat(word2);
+		 * String value = word3;
+		 * addEntry(key.trim(), value.trim()); 
+		 * word1 = word2; 
+		 * word2 = word3; }
+		 */
+
 	}
-	public HashMap getTable(){
+
+	public HashMap getTable() {
 		return this.table;
 	}
+
 	private void addEntry(String key, String value) {
 		ArrayList<String> values = new ArrayList<String>();
 		if (table.containsKey(key)) {
@@ -48,5 +70,5 @@ public class Parser {
 		values.add(value);
 		table.put(key, values);
 	}
-	
+
 }
