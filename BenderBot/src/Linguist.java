e905da1b19d8d264a6ad1438690579694e0d9fd9
cameronfr <catechboy@hotmail.com>
2014-03-25 15:24:37 +0900

    bug fixes

--- BenderBot/src/Linguist.java
+++ BenderBot/src/Linguist.java
@@ -12,7 +12,7 @@ public class Linguist {
 	
 	public Linguist() {
 		keyLength = 2;
-		parser = new Parser();
+		parser = new Parser(keyLength);
 		try {
 			parser.parseFile("/resources/testInput.txt");
 		} catch (FileNotFoundException e) {
@@ -20,20 +20,22 @@ public class Linguist {
 			e.printStackTrace();
 		}
 		table = parser.getTable();
-		//System.out.println(table);
+		System.out.println(table);
 		System.out.println(createSentenceFromKey("name is"));
 	}
+	
 	public String createSentenceFromKey(String startKey) {
-		String sentence = startKey.concat(" ");
+		String sentence = startKey;
 		String key = startKey;
 		int randomIndex;
 		while(table.containsKey(key)) {
-			randomIndex = (int)Math.random() * table.get(key).size();
-			sentence = sentence.concat(table.get(key).get(randomIndex));
+			randomIndex = (int)(Math.random() * table.get(key).size());
+			sentence = sentence.concat(" ").concat(table.get(key).get(randomIndex));
 			key = getKey(sentence);
 		}
 		return sentence;
 	}
+	
 	//returns key at very end of string
 	public String getKey(String text) {
 		String key = "";
