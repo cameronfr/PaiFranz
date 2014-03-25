7ad9cbb2c9179999bfafa7de6c15857040dff403
cameronfr <catechboy@hotmail.com>
2014-03-25 18:17:07 +0900

    slight changes

--- BenderBot/src/Linguist.java
+++ BenderBot/src/Linguist.java
@@ -14,24 +14,26 @@ public class Linguist {
 		keyLength = 2;
 		parser = new Parser(keyLength);
 		try {
-			parser.parseFile("/resources/testInput.txt");
+			parser.parseFile("/resources/BenderQuotes.txt");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		table = parser.getTable();
-		System.out.println(table);
-		System.out.println(createSentenceFromKey("name is"));
+		//System.out.println(table);
+		System.out.println(createTextFromKey("metal ass!", 1000));
 	}
 	
-	public String createSentenceFromKey(String startKey) {
+	public String createTextFromKey(String startKey, int wordLengthLimit) {
 		String sentence = startKey;
 		String key = startKey;
 		int randomIndex;
-		while(table.containsKey(key)) {
+		int iterNum = 0;
+		while(table.containsKey(key) && iterNum < wordLengthLimit) {
 			randomIndex = (int)(Math.random() * table.get(key).size());
 			sentence = sentence.concat(" ").concat(table.get(key).get(randomIndex));
 			key = getKey(sentence);
+			iterNum++;
 		}
 		return sentence;
 	}
