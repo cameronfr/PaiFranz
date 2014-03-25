99a28aca0a7f5f6ba464ae86231c679f4d3c3f82
cameronfr <catechboy@hotmail.com>
2014-03-26 08:37:15 +0900

    can parse some skype files in .csv format

--- BenderBot/src/TextFormatter.java
+++ BenderBot/src/TextFormatter.java
@@ -1,5 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 
@@ -7,7 +10,7 @@ public class TextFormatter {
 
 	public static void main(String[] args) {
 		try {
-			formatFile("/resources/BenderSeason1.txt");
+			formatFile("/resources/CameronMessages.txt");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -17,7 +20,7 @@ public class TextFormatter {
 	public static void formatFile(String path) throws FileNotFoundException {
 		String currentDirectory = new File("").getAbsolutePath();
 		Scanner text = new Scanner(new File(currentDirectory+ path));
-		printBenderLines(text);
+		writeSkypeLines(text);
 	}
 	private static void printBenderLines(Scanner text) {
 		while(text.hasNextLine()) {
@@ -25,18 +28,39 @@ public class TextFormatter {
 			if(line.contains("Bender:")) {
 				String benderLine = line.substring(line.indexOf(" ")+1);
 				benderLine = removeBrackets(benderLine);
-				System.out.println(benderLine);
 			}
 		}
 	}
+	private static void writeSkypeLines(Scanner text) {
+		PrintWriter f0 = null;
+		try {
+			f0 = new PrintWriter(new FileWriter("output.txt"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		while(text.hasNextLine()) {
+			String line = text.nextLine();
+			line = removeSkypeFormatting(line);
+			if(line!=null)f0.println(line);
+		}
+	}
+	
 	private static String removeBrackets(String line) {
 		String fixedLine = line;
 		String bracketSubstring;
 		//if has brackets, fix
-		if(line.indexOf("[")!=-1 && line.indexOf("]")+2 != -1) {
-			bracketSubstring = line.substring(line.indexOf("["), line.indexOf("]")+2);
+		if(line.indexOf("[")>0 && line.indexOf("]") > 0) {
+			bracketSubstring = line.substring(line.indexOf("["), line.indexOf("]")+1);
 			fixedLine = line.replace(bracketSubstring,"");
 		}
 		return fixedLine;
+
+	}
+	private static String removeSkypeFormatting(String line){
+		if(line.contains("&apos;"))line=line.replace("&apos;", ""); 
+		if(line.contains("&quot;"))line=line.replace("&quot;", "");
+		if(line.contains(">") || line.contains("<")) line = null;
+		return line;
 	}
 }
