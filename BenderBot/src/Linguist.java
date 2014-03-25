809bb6c11c67039c781f368573dcc33a06d5eb12
cameronfr <catechboy@hotmail.com>
2014-03-25 12:41:42 +0900

    added dis class which controls the parser

---
+++ BenderBot/src/Linguist.java
@@ -0,0 +1,28 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+
+public class Linguist {
+
+	HashMap<String,ArrayList<String>> table;
+	Parser parser;
+	
+	public Linguist() {
+		parser = new Parser();
+		try {
+			parser.parseFile("/resources/testInput.txt");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		Linguist linguist = new Linguist();
+	}
+
+
+}
