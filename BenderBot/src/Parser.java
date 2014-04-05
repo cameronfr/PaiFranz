e740f38bca3e9b7d0e70a4223d856968b031426c
cameronfr <catechboy@hotmail.com>
2014-04-05 17:08:01 +0900

    cleaned up, transferred some methods to MarkovChain class

--- BenderBot/src/Parser.java
+++ BenderBot/src/Parser.java
@@ -1,74 +1,54 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Parser {
 
-	HashMap<String, ArrayList<String>> table;
-	int markovOrder;
+	private MarkovChain chain;
+	private int markovOrder;
 
-	public Parser(int markovOrderIn) {
-		markovOrder = markovOrderIn;
-		table = new HashMap<String, ArrayList<String>>();
+	public Parser(final MarkovChain chainIn) {
+		chain = chainIn;
+		markovOrder = chain.getKeyLength();
 	}
 
-	public void parseFile(String path) throws FileNotFoundException {
+	public final void parseFile(final String path) {
+		try {
+			simpleReadTextByWord(readFile(path));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private Scanner readFile(final String path) throws FileNotFoundException {
 		String currentDirectory = new File("").getAbsolutePath();
 		Scanner text = new Scanner(new File(currentDirectory + path));
-		simpleReadText(text);
+		return text;
 	}
 
 	// reads from a file with formatted sentences, 2 values
-	private void simpleReadText(Scanner text) {
+	private void simpleReadTextByWord(final Scanner text) {
 		int wordBufferLength = markovOrder + 1;
 		String[] wordBuffer = new String[markovOrder + 1];
 		for (int i = 0; i < wordBufferLength; i++) {
-			if (text.hasNext())
-				wordBuffer[i] = text.next();
+			if (text.hasNext()) {
+					wordBuffer[i] = text.next();
+				}
 		}
 		while (text.hasNext()) {
 			String key = "";
-			String value = wordBuffer[wordBufferLength-1];
-			//last word in buffer is value, not part of key
-			for (int i = 0; i<wordBufferLength-1; i++) {
+			String value = wordBuffer[wordBufferLength - 1];
+			// last word in buffer is value, not part of key
+			for (int i = 0; i < wordBufferLength - 1; i++) {
 				key = key.concat(" ").concat(wordBuffer[i]);
 			}
-			addEntry(key.trim(), value.trim());
-			for (int i = 0; i < wordBufferLength-1; i++) {
-				wordBuffer[i] = wordBuffer[i+1];
+			chain.addEntry(key.trim(), value.trim());
+			for (int i = 0; i < wordBufferLength - 1; i++) {
+				wordBuffer[i] = wordBuffer[i + 1];
 			}
 			wordBuffer[markovOrder] = text.next();
 		}
-
-		/*
-		 * String word1,word2,word3;
-		 *  word1 = text.next(); 
-		 *  word2 = text.next();
-		 * while(text.hasNext()) { 
-		 * word3 = text.next(); 
-		 * String key = word1.concat(" ").concat(word2);
-		 * String value = word3;
-		 * addEntry(key.trim(), value.trim()); 
-		 * word1 = word2; 
-		 * word2 = word3; }
-		 */
-
-	}
-
-	public HashMap getTable() {
-		return this.table;
-	}
-
-	private void addEntry(String key, String value) {
-		ArrayList<String> values = new ArrayList<String>();
-		if (table.containsKey(key)) {
-			values = table.get(key);
-		}
-		values.add(value);
-		table.put(key, values);
 	}
 
 }
