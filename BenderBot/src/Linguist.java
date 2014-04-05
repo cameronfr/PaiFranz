e15f46e13acb0a9eac808cc70b7dcf1611503246
cameronfr <catechboy@hotmail.com>
2014-04-05 17:08:46 +0900

    cleaned up, transferred some methods to MarkovChain class

--- BenderBot/src/Linguist.java
+++ BenderBot/src/Linguist.java
@@ -2,59 +2,37 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-
-
 public class Linguist {
 
-	HashMap<String,ArrayList<String>> table;
-	Parser parser;
-	int markovOrder;
-	
+	private Parser parser;
+	private MarkovChain chain;
+	private int keyLength = 3;
+	private static final int TEXT_LENGTH = 1000;
+
 	public Linguist() {
-		markovOrder = 2;
-		parser = new Parser(markovOrder);
-		try {
-			parser.parseFile("/resources/CameronMessages.txt");
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		table = parser.getTable();
+		chain = new MarkovChain(keyLength);
+		parser = new Parser(chain);
+		parser.parseFile("/resources/NaveenMessages.txt");
 		//System.out.println(table);
-		System.out.println(createTextFromKey(randomKey(), 1000));
+		System.out.println(createTextFromKey(chain.randomKey(), TEXT_LENGTH));
 	}
-	
-	public String createTextFromKey(String startKey, int wordLengthLimit) {
+
+	public final String createTextFromKey(final String startKey, final int wordLengthLimit) {
+		HashMap<String, ArrayList<String>> table = chain.getTable();
 		String sentence = startKey;
 		String key = startKey;
 		int randomIndex;
 		int iterNum = 0;
-		while(table.containsKey(key) && iterNum < wordLengthLimit) {
-			randomIndex = (int)(Math.random() * table.get(key).size());
+		while (table.containsKey(key) && iterNum < wordLengthLimit) {
+			randomIndex = (int) (Math.random() * table.get(key).size());
 			sentence = sentence.concat(" ").concat(table.get(key).get(randomIndex));
-			key = getKey(sentence);
+			key = chain.keyFromText(sentence);
 			iterNum++;
 		}
 		return sentence;
 	}
-	
-	//returns key at very end of string
-	private String getKey(String text) {
-		String key = "";
-		String words[] = text.split(" ");
-		for(int i = words.length-markovOrder; i<words.length;i++){
-			key = key.concat(words[i]).concat(" ");
-		}
-		return key.trim();
-	}
-	public String randomKey() {
-		int rand = (int)(Math.random() * table.keySet().size());
-		return (String) table.keySet().toArray()[rand];
-	}
-	
-	public static void main(String[] args) {
+
+	public static void main(final String[] args) {
 		Linguist linguist = new Linguist();
 	}
-
-
 }
