a6c6e079223aec3cbd00f6553062e6c63e85a7ee
cameronfr <catechboy@hotmail.com>
2014-04-05 17:07:31 +0900

    cleaned up

--- BenderBot/src/TextFormatter.java
+++ BenderBot/src/TextFormatter.java
@@ -5,33 +5,32 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
+class TextFormatter {
 
-public class TextFormatter {
-
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		try {
-			formatFile("/resources/CameronMessages.txt");
+			formatFile("/resources/DavidMessages.txt");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	public static void formatFile(String path) throws FileNotFoundException {
+	public static void formatFile(final String path) throws FileNotFoundException {
 		String currentDirectory = new File("").getAbsolutePath();
-		Scanner text = new Scanner(new File(currentDirectory+ path));
+		Scanner text = new Scanner(new File(currentDirectory + path));
 		writeSkypeLines(text);
 	}
-	private static void printBenderLines(Scanner text) {
-		while(text.hasNextLine()) {
+	private static void printBenderLines(final Scanner text) {
+		while (text.hasNextLine()) {
 			String line = text.nextLine();
-			if(line.contains("Bender:")) {
-				String benderLine = line.substring(line.indexOf(" ")+1);
+			if (line.contains("Bender:")) {
+				String benderLine = line.substring(line.indexOf(" ") + 1);
 				benderLine = removeBrackets(benderLine);
 			}
 		}
 	}
-	private static void writeSkypeLines(Scanner text) {
+	private static void writeSkypeLines(final Scanner text) {
 		PrintWriter f0 = null;
 		try {
 			f0 = new PrintWriter(new FileWriter("output.txt"));
@@ -39,28 +38,29 @@ public class TextFormatter {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		while(text.hasNextLine()) {
+		while (text.hasNextLine()) {
 			String line = text.nextLine();
 			line = removeSkypeFormatting(line);
-			if(line!=null)f0.println(line);
+			if (line != null) f0.println(line);
 		}
 	}
-	
-	private static String removeBrackets(String line) {
+
+	private static String removeBrackets(final String line) {
 		String fixedLine = line;
 		String bracketSubstring;
 		//if has brackets, fix
-		if(line.indexOf("[")>0 && line.indexOf("]") > 0) {
-			bracketSubstring = line.substring(line.indexOf("["), line.indexOf("]")+1);
-			fixedLine = line.replace(bracketSubstring,"");
+		if (line.indexOf("[") > 0 && line.indexOf("]") > 0) {
+			bracketSubstring = line.substring(line.indexOf("["), line.indexOf("]") + 1);
+			fixedLine = line.replace(bracketSubstring, "");
 		}
 		return fixedLine;
 
 	}
-	private static String removeSkypeFormatting(String line){
-		if(line.contains("&apos;"))line=line.replace("&apos;", ""); 
-		if(line.contains("&quot;"))line=line.replace("&quot;", "");
-		if(line.contains(">") || line.contains("<")) line = null;
+	private static String removeSkypeFormatting(final String lineIn) {
+		String line = lineIn;
+		if (line.contains("&apos;")) line = line.replace("&apos;", "");
+		if (line.contains("&quot;")) line = line.replace("&quot;", "");
+		if (line.contains(">") || line.contains("<")) line = null;
 		return line;
 	}
 }
